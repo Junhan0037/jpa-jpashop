71c9bfd85edda79b3d7bc11f927a2b9ec0a4c5ce
Junhan0037 <wnsgks0037@naver.com>
2020-06-10 17:47:24 +0900

    ADD 주문 조회 V1 ,V2

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,6 +15,7 @@ public class Delivery {
     @Column(name = "delivery_id")
     private Long id;
 
+    @JsonIgnore // 양방향 관계에서 API 무한 루프 방지
     @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
     private Order order;
 
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -22,6 +23,7 @@ public class Member {
     @Embedded
     private Address address;
 
+    @JsonIgnore // 양방향 관계에서 API 무한 루프 방지
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 
