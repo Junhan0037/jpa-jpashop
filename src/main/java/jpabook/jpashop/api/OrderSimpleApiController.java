8a6a106c0bc4997628f6ed7150b2ba276840f246
Junhan0037 <wnsgks0037@naver.com>
2020-06-11 16:44:54 +0900

    ADD 주문 조회 V3, V4 (fetch join, JPA DTO)

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -5,6 +5,8 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,6 +28,7 @@ import java.util.stream.Collectors;
 public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
 
     /**
      * V1. 엔티티 직접 노출
@@ -33,7 +36,7 @@ public class OrderSimpleApiController {
      * - 양방향 관계 문제 발생 -> @JsonIgnore
      */
     @GetMapping("/api/v1/simple-orders")
-    public List<Order> ordersV1() {
+    public List<Order> ordersV1() { // 엔티티를 직접 노출 (엔티티를 직접 노출하는 것은 좋지 않다!)
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
         for (Order order : all) {
             order.getMember().getName(); // Lazy 강제 초기화
@@ -43,7 +46,7 @@ public class OrderSimpleApiController {
     }
 
     @GetMapping("/api/v2/simple-orders")
-    public List<SimpleOrderDto> ordersV2() {
+    public List<SimpleOrderDto> ordersV2() { // 엔티티를 DTO로 변환 (N+1문제 발생!)
         List<Order> orders = orderRepository.findAllByString(new OrderSearch());
         List<SimpleOrderDto> result = orders.stream()
                 .map(order -> new SimpleOrderDto(order))
@@ -51,6 +54,22 @@ public class OrderSimpleApiController {
 
         return result;
     }
+
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() { // 패치 조인 최적화 (쿼리 1번, 패치 조인으로 이미 조회된 상태 이므로 지연로딩X)
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        List<SimpleOrderDto> result = orders.stream()
+                .map(order -> new SimpleOrderDto(order))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @GetMapping("/api/v4/simple-orders") // JPA에서 DTO로 바로 조회
+    public List<OrderSimpleQueryDto> ordersV4() {
+        return orderSimpleQueryRepository.findOrderDtos();
+    }
+
 
     @Data
     static class SimpleOrderDto {
