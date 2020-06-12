ef879e25ff832c13213c24749878476349da6fb3
Junhan0037 <wnsgks0037@naver.com>
2020-06-12 16:10:41 +0900

    ADD 주문 조회 V2: 엔티티를 DTO로 변환

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,14 +1,19 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,13 +21,9 @@ public class OrderApiController {
 
     private final OrderRepository orderRepository;
 
-    /**
-     * V1. 엔티티 직접 노출
-     * - Hibernate5Module 모듈 등록, LAZY=null 처리
-     * - 양방향 관계 문제 발생 -> @JsonIgnore
-     */
     @GetMapping("/api/v1/orders")
-    public List<Order> ordersV1() {
+    public List<Order> ordersV1() { // 엔티티 직접 노출
+
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
         for (Order order : all) {
             order.getMember().getName(); // Lazy 강제 초기화
@@ -30,7 +31,56 @@ public class OrderApiController {
             List<OrderItem> orderItems = order.getOrderItems();
             orderItems.stream().forEach(orderItem -> orderItem.getItem().getName()); // Lazy 강제 초기화
         }
+
         return all;
+    }
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() { // 엔티티를 DTO로 변환 (지연로딩으로 너무 많은 실행, 1+N문제 발생)
+
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> collect = orders.stream()
+                .map(order -> new OrderDto(order))
+                .collect(Collectors.toList());
+
+        return  collect;
+    }
+
+
+    @Data
+    static class OrderDto {
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems; // 엔티티를 직접 노출하지 않기 위해 DTO 재생성
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Data
+    static class OrderItemDto {
+
+        private String itemName; // 상품 명
+        private int orderPrice; // 주문 가격
+        private int count; // 주문 수량
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
     }
 
 }
